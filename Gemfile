dde21f0a8314f8c8a3bcc0c9b8a3ebc055d03cde
alexhartley <alexhartley@gmail.com>
2015-10-23 23:06:46 +0100

    Make it work with active record and add basic tests

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ gem "activerecord", require: "active_record"
 gem "sqlite3", "~> 1.3", ">= 1.3.11"
 gem "serialport", "~> 1.3", ">= 1.3.1"
 gem "json", "~> 1.8", ">= 1.8.3"
-gem "slop", "~> 3.6"
-gem 'rspec', '~> 3.3'
+gem 'slop', '~> 4.2'
+gem "rspec", "~> 3.3"
+gem "database_cleaner", "~> 1.5", ">= 1.5.1"
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,40 @@
 class User < ActiveRecord::Base
   has_many :hs_sessions
+
+  def self.process_uid(uid)
+    user = User.find_by(uid: uid)
+    user.nil? ? create_user(uid) : user.process_session
+  end
+
+  def self.create_user(uid)
+    puts "creating user"
+    User.create(uid: uid)
+  end
+
+  def process_session
+    puts "Processing session"
+    if hs_sessions.empty? || hs_sessions.last.timeout?
+      create_new_session
+    else
+      sign_out_user
+    end
+  end
+
+  private
+
+  def create_new_session
+    hs_sessions.create(timein: Time.now)
+    puts "Signed in"
+  end
+
+  def sign_out_user
+    hs_sessions.last.update_attribute(:timeout, Time.now)
+    puts "Signed out"
+    work_out_diff
+  end
+
+  def work_out_diff
+    ##### TODO #####
+    true
+  end
 end
