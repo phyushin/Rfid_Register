c2fbb0bcd0e138e0a851cf8eb9e154d1e250fbb6
alexhartley <alexhartley@gmail.com>
2015-11-01 18:47:38 +0000

    Update the way random uid is generated

--- spec/hs_session_spec.rb
+++ spec/hs_session_spec.rb
@@ -5,14 +5,13 @@ RSpec.describe HsSession do
     time_in = Time.local(2015, 10, 1, 8, 0, 0)
 
     [4.hours, 0.5.hours, 3.5.hours, 0.25.hours].each do |time|
-      uid = (0...50).map { ('0'..'9').to_a[rand(26)] }.join
+      uid = SecureRandom.hex
 
       Timecop.freeze(time_in) do
         User.process_uid(uid)
       end
 
       user = User.find_by(uid: uid)
-      @uid_new = (uid.to_i + 1).to_s
 
       Timecop.freeze(time_in + time) do
         user.process_session
